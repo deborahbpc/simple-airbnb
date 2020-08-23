36837f67309d9cf6155f47adbab64f54c32c66fa
Deborah Carvalho <deborah.bpc@gmail.com>
2020-08-23 11:45:09 -0300

    listing in index

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,3 +1,5 @@
 class FlatsController < ApplicationController
-  def index; end
+  def index
+    @flats = Flat.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'flats/index'
+  # get 'flats/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :flats
+
+  root to: 'flats#index'
 end
